95cd321d8a6a8b1511668b31d4e266d0beb267dc
pharma <mostafa.mohammed.sayed.awad@gmail.com>
2021-06-17 14:53:21 +0200

    writing from scratch instead of Udacity code

---
+++ app/src/main/java/com/example/android/mymiwok/NumbersMainActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.mymiwok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NumbersMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.activity_numbers_main);
+    }
+}
