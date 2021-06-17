52d074bdf4a4104119adb0b282954da8092bcd0b
pharma <mostafa.mohammed.sayed.awad@gmail.com>
2021-06-17 20:55:41 +0200

    displaying one data item of a list in our Numbers Activity.

--- app/src/main/java/com/example/android/mymiwok/NumbersMainActivity.java
+++ app/src/main/java/com/example/android/mymiwok/NumbersMainActivity.java
@@ -3,6 +3,11 @@ package com.example.android.mymiwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersMainActivity extends AppCompatActivity {
 
@@ -11,5 +16,29 @@ public class NumbersMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         
         setContentView(R.layout.activity_numbers_main);
+
+        //creating an ArrayList of the data we want to display in Numbers Activity then
+        //adding first data item in a TextView then making this Text view a child of
+        // the Linear Layout of the Numbers category
+
+        ArrayList <String> words = new ArrayList <String> ();
+
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        LinearLayout rootView = findViewById(R.id.rootView);
+
+        TextView textView = new TextView(this);
+        textView.setText(words.get(0));
+        rootView.addView(textView);
+
     }
 }
