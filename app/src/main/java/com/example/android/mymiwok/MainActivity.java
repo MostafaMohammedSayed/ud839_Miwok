a5dcbfcd33913f6d5ff6efc90f95d2631d727728
pharma <mostafa.mohammed.sayed.awad@gmail.com>
2021-06-17 17:13:59 +0200

    setting a Click listener on category numbers with many lines of code'

--- app/src/main/java/com/example/android/mymiwok/MainActivity.java
+++ app/src/main/java/com/example/android/mymiwok/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NumbersclickListener numbersclickListener= new NumbersclickListener();
+
+        TextView numbersTextview = (TextView) findViewById(R.id.numbers);
+
+        numbersTextview.setOnClickListener(numbersclickListener);
     }
 
-    public void openNumbersList (View view){
-        //TODO:write code here!
-    }
+//    public void openNumbersList (View view){
+//        //Now on click the numbers screen opens (via xml onClick & java intent to start numbers)
+//        Intent intent = new Intent (this, NumbersMainActivity.class);
+//        startActivity(intent);
+//    }
 }
