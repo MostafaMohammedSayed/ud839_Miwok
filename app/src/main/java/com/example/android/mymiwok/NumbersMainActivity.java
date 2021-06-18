cdc5b9f0b7de0b8f4252f004ed48e07e90cfc755
pharma <mostafa.mohammed.sayed.awad@gmail.com>
2021-06-18 14:29:30 +0200

    try adapter with ArrayList<words> but fail

--- app/src/main/java/com/example/android/mymiwok/NumbersMainActivity.java
+++ app/src/main/java/com/example/android/mymiwok/NumbersMainActivity.java
@@ -24,24 +24,27 @@ public class NumbersMainActivity extends AppCompatActivity {
         //adding first data item in a TextView then making this Text view a child of
         // the Linear Layout of the Numbers category
 
-        ArrayList <String> words = new ArrayList <String> ();
+        ArrayList <Word> words = new ArrayList <Word> ();
 
-        words.add("one");
-        words.add("two");
-        words.add("three");
-        words.add("four");
-        words.add("five");
-        words.add("six");
-        words.add("seven");
-        words.add("eight");
-        words.add("nine");
-        words.add("ten");
+        words.add(new Word("one","lutti"));
+        words.add(new Word("two","otiiko"));
+        words.add(new Word("three","tolookosu"));
+        words.add(new Word("four","oyyisa"));
+        words.add(new Word("five","massokka"));
+        words.add(new Word("six","temmokka"));
+        words.add(new Word("seven","kenekaku"));
+        words.add(new Word("eight","kawinta"));
+        words.add(new Word("nine","wo'e"));
+        words.add(new Word("ten","na'aacha"));
 
         //added array adapter to optimize memory usage by the app
         //forgot to say we totally changed the layout of Numbers in the xml file
 
-        ArrayAdapter <String> arrayAdapter = new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1,words);
-
+        ArrayAdapter <Word> arrayAdapter = new ArrayAdapter<String> (this,R.layout.list_item,words);
+        //but the previous line gives a compiler error that says that the arrayAdapter different constructors
+        //do no match with the parameters that we provided, the problem seems to be in the last parameter (words)
+        //as the error says the constructor takes an ArrayList<String> not ArrayList<words>. Looks like we are
+        //gonna improvise and make our own adapter (To be Continued...)
         ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(arrayAdapter);
     }
